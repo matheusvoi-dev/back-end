ccedf15eb41a4b0f0933c0db5ef405c70f80ca62
matheusvoi-dev <matheus.voi@escola.pr.gov.br>
2026-08-19 15:33:05 +0000

    contra

---
+++ teste.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class teste {
+    public static void main(String[] args) {
+        System.out.println("######BANCO######");
+        System.out.println("#####Digite 1 para saque#####");
+        System.out.println("####Digite 2 para depositar####");
+        System.out.println("###Digite 3 para extrato###");
+        System.out.println("##Digite 4 para sair##");
+    }
+}
